982f4f952ad96010f344f92391c684593a17805c
DoanTrongVinh <clonevinh95@gmail.com>
2022-01-17 16:20:54 +0700

    enter

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        String strHoten = "";
+        System.out.println("Nhap ho ten: ");
+        strHoten = sc.nextLine();
+        System.out.println("Xin chào " + strHoten);
     }
 }
